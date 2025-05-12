c8e52504a527b7f3a746f288c5d9a2a5c9496f03
unknown <azzurraegidi8@gmail.com>
2025-05-12 16:36:53 +0200

    Perfezionate immagini del Carosello

--- src/components/AsRomaNavbar/Sponsor.tsx
+++ src/components/AsRomaNavbar/Sponsor.tsx
@@ -2,7 +2,7 @@ import sponsorSeason from "../../assets/SponsorSeason.png";
 import sponsorAdidas from "../../assets/SponsorAdidas.png";
 
 const Sponsor = () => (
-  <div className="flex items-center gap-4 ml-[88rem] justify-between">
+  <div className="flex items-center mr-8 gap-4 ml-[88rem] justify-between">
     <img
       className="w-12 h-auto"
       src={sponsorSeason}
--- src/components/Carosello.tsx
+++ src/components/Carosello.tsx
@@ -1,31 +1,65 @@
 import React from "react";
+import slide1 from "../assets/assetsCarosello/stadio.jpg";
+import slide2 from "../assets/assetsCarosello/ranieri.jpg";
+import slide3 from "../assets/assetsCarosello/giugliano.jpg";
 
 const Carosello = () => {
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
       <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
 
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="/immagini/slide1.jpg" className="d-block w-100" alt="Slide 1" />
+          <img src={slide1} className="d-block w-100" alt="Slide 1" />
         </div>
         <div className="carousel-item">
-          <img src="/immagini/slide2.jpg" className="d-block w-100" alt="Slide 2" />
+          <img src={slide2} className="d-block w-100" alt="Slide 2" />
         </div>
         <div className="carousel-item">
-          <img src="/immagini/slide3.jpg" className="d-block w-100" alt="Slide 3" />
+          <img src={slide3} className="d-block w-100" alt="Slide 3" />
         </div>
       </div>
 
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
